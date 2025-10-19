7b0d3b4fd56cc4fe16f7ad988da764ecdf391d8b
Wilsiman Evangelista <wilsiman.evangelista.ifes@gmail.com>
2025-10-18 22:50:08 -0300

    Criacao do metodo getIdade() e toString()

--- Pessoa.java
+++ Pessoa.java
@@ -21,4 +21,13 @@ public class Pessoa {
         return nasc;
     }
 
+    public int getIdade(Data hoje) {
+        if (this.nasc.getMes() > hoje.getMes()) { return hoje.getAno() - this.nasc.getAno() - 1; }
+        else if (this.nasc.getMes() < hoje.getMes()) { return hoje.getAno() - this.nasc.getAno(); }
+        else if (this.nasc.getDia() < hoje.getDia()) { return hoje.getAno() - this.nasc.getAno() - 1; }
+        else { return hoje.getAno() - this.nasc.getAno() - 1; }
+    }
+
+    public String toString() { return this.nome + " - CPF: " + this.cpf; }
+
 }
